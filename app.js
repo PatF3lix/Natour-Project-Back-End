b0f6df56d38e5752f9a5887a5add40cd796b7fc2
PatF3lix <pat_pg@hotmail.fr>
2023-12-14 20:21:02 +0000

    installing eslint and prettier dependencies

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
 /**This file has the main purpose of holding all the Express configuration needed to make the application */
-
 const app = express();
-
 //Middlewares for all the routes
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -15,7 +12,7 @@ app.use(express.json());
 //Serving Static files
 app.use(express.static(`${__dirname}/public`));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   console.log('Hello from the middleware :D');
   next();
 });

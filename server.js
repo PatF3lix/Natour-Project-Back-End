c25710821be706c38b79a54d8acea783d69fc7e8
PatF3lix <pat_pg@hotmail.fr>
2023-12-15 16:28:57 +0000

    connected mongo db to the project , using a layer of abstraction called moongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 

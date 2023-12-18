9208da52e06e943ccd63c1ab7a35c60b455f5f4f
PatF3lix <pat_pg@hotmail.fr>
2023-12-18 14:26:56 +0000

    implemented document, query, aggregation middleware to the tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -53,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -63,6 +70,46 @@ const tourSchema = new mongoose.Schema(
 //virtual property
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+//QUERY MIDDLEWARE
+// '/^find/' is a regular expression which will permits this function to trigger
+// not only for '.find()', but also for all func starting with find:
+//'.findOne(), .findOneAndDelete, .findOneAndUpdate, etc..'
+tourSchema.pre(/^find/, function (next) {
+  //the this keyword will point to the query not the tourSchema object
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(docs);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+//exclude the secret tours from output using aggregation middle ware
+tourSchema.pre('aggregation', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model('Tours-simple', tourSchema);

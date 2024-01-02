ded7cee0907349df5e6aefbaa0adb4a9ef9bba0b
PatF3lix <pat_pg@hotmail.fr>
2024-01-02 12:52:40 +0000

    checkpoint after installing parcel-bundler  axios and polyfill

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
-/* eslint-disable */
+/*eslint-disable*/
+import axios from 'axios';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   // console.log(email, password);
   try {
     const res = await axios({
@@ -23,10 +24,3 @@ const login = async (email, password) => {
     alert(error.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});

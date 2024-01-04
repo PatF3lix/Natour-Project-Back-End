cbb364a0ade53f046d809bf2c6993d63612c9989
PatF3lix <pat_pg@hotmail.fr>
2024-01-04 21:09:54 +0000

    implemented logic to send email during production by using smtp-relay.brevo.com  as an intermediate

--- utils/email.js
+++ utils/email.js
@@ -15,10 +15,18 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       //sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        host: 'smtp-relay.brevo.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_LOGIN_BREVO, // generated ethereal user
+          pass: process.env.EMAIL_BREVO_SMTP_KEY, // generated ethereal password
+        },
+      });
     }
 
-    return nodemailer.newTransport({
+    return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
       auth: {
@@ -31,14 +39,11 @@ module.exports = class Email {
   async send(template, subject) {
     //send the actual email
     // 1) Render HTML based on template
-    const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
-      {
-        firstName: this.firstName,
-        url: this.url,
-        subject,
-      },
-    );
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
     //2) Define email options
     const mailOptions = {
@@ -46,7 +51,7 @@ module.exports = class Email {
       to: this.to,
       subject: subject,
       html,
-      text: htmlToText.htmlToText.fromString(html),
+      text: htmlToText.convert(html),
     };
 
     //3) Create atransport and send email
@@ -55,5 +60,12 @@ module.exports = class Email {
 
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
   }
 };

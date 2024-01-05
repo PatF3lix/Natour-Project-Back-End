3cdb43f360c9f5ebdeb3487f8629e8b35792579f
PatF3lix <pat_pg@hotmail.fr>
2024-01-05 18:21:55 +0000

    succes url corrected on completed transaction on bookingController.js -- transactions with stripe are functional

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,9 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,

ba4ceb44d39540914373bdb94109d29efce55920
PatF3lix <pat_pg@hotmail.fr>
2024-01-06 10:53:06 +0000

    implemented api shutdown on received sigterm signal

--- server.js
+++ server.js
@@ -38,5 +38,13 @@ mongoose
         process.exit(1);
       });
     });
+
+    process.on('SIGTERM', () => {
+      console.log('SIGTERM RECEIVED. shutting down grafeully');
+      server.close(() => {
+        console.log('Process terminated!');
+        process.exit(1);
+      });
+    });
   })
   .catch((err) => console.log(err));
